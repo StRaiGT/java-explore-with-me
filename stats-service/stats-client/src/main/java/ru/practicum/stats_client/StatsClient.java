76663fa8eb81dd79fa67e46ad97960414f579a18
Kirill Baturin <kibat@yandex.ru>
2023-03-19 20:50:38 +0300

    fix naming ENV

--- stats-service/stats-client/src/main/java/ru/practicum/stats_client/StatsClient.java
+++ stats-service/stats-client/src/main/java/ru/practicum/stats_client/StatsClient.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.stats_common.CommonUtils;
@@ -19,10 +18,9 @@ import java.util.Map;
 @Slf4j
 public class StatsClient extends BaseClient {
     @Autowired
-    public StatsClient(@Value("${stats-service.url}") String serverUrl, RestTemplateBuilder builder) {
+    public StatsClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(builder
                 .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
-                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
                 .build()
         );
     }
